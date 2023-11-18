62c5f5022a7f36c8ddde97898679a088345f01bb
nilobstacle <lorem.ipsum.io@outlook.jp>
2023-11-19 00:13:42 +0900

    add event handler

--- src/commands.rs
+++ src/commands.rs
@@ -6,12 +6,19 @@ use crate::{
 #[poise::command(slash_command)]
 pub async fn ping(ctx: Context<'_>) -> Result<(), ApplicationError> {
 
-    poise::send_reply(ctx, |f| f
+    poise::send_reply(ctx, |rep| rep
         .embed(|e| e
-            .title("Pong.")
+            .title("Pong")
             .color((230, 126, 34))
+            .timestamp("2017-01-03T23:00:00Z")
         )
         .ephemeral(true)
     ).await?;
+    Ok(())
+}
+
+#[poise::command(slash_command)]
+pub async fn debug(ctx: Context<'_>) -> Result<(), ApplicationError> {
+    ctx.reply(format!("your id: {}", ctx.author().id)).await?;
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -5,40 +5,55 @@ use poise::serenity_prelude::{
     GuildId, 
     Activity
 };
-use commands::ping;
+use commands::{
+    ping,
+    debug
+};
 
-#[derive(thiserror::Error, Debug)]
-pub enum ApplicationError {
-    #[error("{0}")]
-    Serenity(#[from] poise::serenity_prelude::Error)
-}
-
+pub type ApplicationError = Box<dyn std::error::Error + Send + Sync>;
 pub type Context<'a> = poise::Context<'a, (), ApplicationError>;
+
+
+async fn event_handler(
+    ctx: &poise::serenity_prelude::Context,
+    event: &poise::Event<'_>,
+    _framework: poise::FrameworkContext<'_, (), ApplicationError>
+) -> Result<(), ApplicationError> {
+    match event {
+        poise::Event::Ready { data_about_bot } => {
+            let user = &data_about_bot.user;
+            println!("Bot: {} is all set.", user.tag());
+
+            ctx.set_activity(Activity::listening("/debug")).await;
+        }
+        _ => {}
+    }
+    Ok(())
+}
 
 
 #[tokio::main]
 async fn main() {
     dotenv::dotenv().ok();
-    let token = env::var("TOKEN").expect("TOKEN");
-    let guild_id = env::var("GUILD").expect("GUILD").parse::<GuildId>().unwrap();
+    let token = env::var("TOKEN").expect("TOKEN should be set.");
+    let guild_id = env::var("GUILD").expect("GUILD should be set.").parse::<GuildId>().unwrap();
 
     let framework = poise::Framework::builder()
         .options(poise::FrameworkOptions { 
             commands: vec![
-                ping()
+                ping(),
+                debug()
             ],
+            event_handler: |ctx, event, framework, _| {
+                Box::pin(event_handler(ctx, event, framework))
+            },
             ..Default::default()
         })
          .token(&token)
          .intents(GatewayIntents::non_privileged())
-         .setup(move |ctx, _ready, framework| {
+         .setup(move |ctx, _, framework| {
             Box::pin(async move {
                 poise::builtins::register_in_guild(ctx, &framework.options().commands, guild_id).await?;
-
-                let user = ctx.http.get_current_user().await?;
-                println!("Bot: {} is all set.", user.tag());
-                
-                ctx.set_activity(Activity::listening("/play")).await;
                 Ok(())
             })
         });
@@ -46,5 +61,5 @@ async fn main() {
     println!("Token: {}", token);
     println!("Guild: {}", guild_id);
 
-    framework.run().await.unwrap()
+    framework.run().await.unwrap();
 }
